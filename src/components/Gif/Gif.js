dbb7fdc8d8fa1f9802a1bc3d80c5256e8ad198f3
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2022-05-24 20:47:47 +0100

    refactor

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './gifs.scss'
 
 export default function Gif ({title, url, id}){
     return (
-        <div>
-            <h4>{title}</h4>
+        <div className="gifContainer">
+            <h4 className="title">{title}</h4>
             <img src={url} alt={title}/>
         </div>
         )
